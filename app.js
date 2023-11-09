c3e9a3c8df53391a7f0afde741660f49b2bd60c6
saiyed9974 <saiyed9974@gmail.com>
2023-11-09 15:37:39 +0530

    create element dynamically

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ btnTranslate.addEventListener("click", function clickEventhandler(){
     output.innerText = " sdfsfdf(banana) "+ txtInput.value; 
 
 })
+
+document.body.onload = addElement;
+function addElement(){
+
+    var newdiv = document.createElement("div");
+
+    var newcontent = document.createTextNode("I love javascript");
+
+    newdiv.appendChild(newcontent);
+
+    var currenttag = document.getElementById("div1");
+
+    document.body.insertBefore(newdiv,currenttag);
+
+}
